48a4b4f7613d002faef4669823510e2ed6fe1601
Cian Levy <cian.b.levy@gmail.com>
2019-02-26 19:47:19 +0000

    Converted station information table to listview. Also slightly broke the appbar

--- app/src/main/java/com/example/twainz/MainActivity.java
+++ app/src/main/java/com/example/twainz/MainActivity.java
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         // Create the adapter that will return a fragment for each of the two
         // primary sections of the activity.
--- app/src/main/java/com/example/twainz/stationInformationActivity.java
+++ app/src/main/java/com/example/twainz/stationInformationActivity.java
@@ -1,5 +1,6 @@
 package com.example.twainz;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
@@ -9,12 +10,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Vector;
 
@@ -26,11 +30,7 @@ public class stationInformationActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.activity_station_information, container, false);
-
-        //Initialise the layout global
-        layout = rootView.findViewById(R.id.trainListLayout);
-        layout.setStretchAllColumns(true);
+        rootView = inflater.inflate(R.layout.station_view_fragment, container, false);
 
         //Initialise the trainFetcher. Object should already contain the string of the specified station
         trainFetcher tf = new trainFetcher();
@@ -48,110 +48,55 @@ public class stationInformationActivity extends Fragment {
         displayRefresh.setText("Updated at " + time.format(calender.getTime()));
 
 
-        //Declaring a tablerow to contain the textviews labelling the columns
-        TableRow columnTitles = new TableRow(rootView.getContext());
-
-        //Creating a textview to display the label for the arrival time column
-        TextView arrivalColumn = new TextView(rootView.getContext());
-        arrivalColumn.setText("Arrival Time         ");
-        arrivalColumn.setTextSize(20);
-        arrivalColumn.setWidth(220);
-
-        //Creating a textview to display the label for the destination column
-        TextView destinationColumn = new TextView(rootView.getContext());
-        destinationColumn.setText("Destination");
-        destinationColumn.setTextSize(20);
-
-        //Adding the textviews to the row
-        columnTitles.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-        columnTitles.addView(arrivalColumn);
-        columnTitles.addView(destinationColumn);
-
-        //Adding the row to the layout
-        TableLayout l = rootView.findViewById(R.id.linearLayout);
-        l.addView(columnTitles);
-        l.setStretchAllColumns(true);
-
         //Using the trainFetcher to retrieve the data for the station
         Vector<train> trains = tf.getTrains();
 
-        //For each train add a new UI element
-        for (train t : trains)
-            addTrain(t);
+        ArrayList<train> list = new ArrayList<train>(trains);
 
+        trainAdapter adapter = new trainAdapter(rootView.getContext(), list);
+
+        ListView lv =  rootView.findViewById(R.id.trainListView);
+
+        lv.setAdapter(adapter);
 
 
         return rootView;
 
     }
+}
 
-    private void addTrain(train t){
-        TableLayout subTable = new TableLayout(rootView.getContext());
-        subTable.setStretchAllColumns(true);
+class trainAdapter extends ArrayAdapter<train> {
 
-        //Adding a border to the subTables
-        GradientDrawable border = new GradientDrawable();
-        border.setColor(0xFFFFFFFF); //white background
-        border.setStroke(4, 0xFF000000); //black border with full opacity
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-            subTable.setBackgroundDrawable(border);
-        } else {
-            subTable.setBackground(border);
-        }
-
-        //Declaring the upper row and changing the color
-        TableRow row2 = new TableRow(rootView.getContext());
-        int color = (128 & 0xff) << 24 | (0 & 0xff) << 16 | (128 & 0xff) << 8 | (128 & 0xff);
-        row2.setBackgroundColor(color);
-
-        //TextView for the arrival time
-        TextView arrivalTime = new TextView(rootView.getContext());
-        arrivalTime.setText(t.getArrivalTime());
-        arrivalTime.setTextSize(textSize);
-        arrivalTime.setWidth(40);
-
-
-        //TextView for the delay duration
-        TextView delay = new TextView(rootView.getContext());
-        delay.setText("+ " + String.valueOf(t.getDelay()));
-        delay.setTextSize(textSize);
-        delay.setWidth(40);
-
-        //Set the delay TextView font color based on the delay
-        if (t.getDelay() > 0)
-            delay.setTextColor(Color.RED);
-        else if (t.getDelay() < 0){
-            delay.setText("- " + String.valueOf(-1 * t.getDelay()));
-            delay.setTextColor(Color.GREEN);
-        }
-
-        //TextView for the train destination
-        TextView destination = new TextView(rootView.getContext());
-        destination.setText("To " + t.getDestination());
-        destination.setTextSize(textSize);
-        destination.setWidth(200);
-
-        //TextView for the type of train
-        TextView type = new TextView(rootView.getContext());
-        type.setText(t.getType());
-        type.setTextSize(textSize);
-        type.setWidth(200);
-
-        //Creating a new row and adding the delay and train destination
-        TableRow row1 = new TableRow(rootView.getContext());
-        row1.addView(delay);
-        row1.addView(destination);
-
-        //Adding the arrival time and train type to the second row
-        row2.addView(arrivalTime);
-        row2.addView(type);
-
-        //Add the rows to the subtable
-        subTable.addView(row2);
-        subTable.addView(row1);
-
-        //Add the subtable to the parent layout
-        layout.addView(subTable);
+    public trainAdapter(Context context, ArrayList<train> trains) {
+        super(context, 0, trains);
 
     }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        train t = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.station_information_row, parent, false);
+        }
+
+        TextView tempView = convertView.findViewById(R.id.trainTypeView);
+        tempView.setText(t.getType());
+        tempView.setTag(position); //Tag the button with it's position in the list
+
+        tempView = convertView.findViewById(R.id.trainDestinationView);
+        tempView.setText(t.getDestination());
+        tempView.setTag(position); //Tag the button with it's position in the list
+
+        tempView = convertView.findViewById(R.id.arrivalTimeView);
+        tempView.setText(t.getArrivalTime());
+        tempView.setTag(position); //Tag the button with it's position in the list
+
+
+
+
+        return convertView;
+
+    }
+
 }
