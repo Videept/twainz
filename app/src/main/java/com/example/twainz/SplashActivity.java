783d61fc92cae7249c94662a29033d20a0f5f1ea
Preetam Singhvi <preetamsinghvi@gmail.com>
2019-04-08 19:08:35 +0100

    Splash Screen

--- app/src/main/java/com/example/twainz/SplashActivity.java
+++ app/src/main/java/com/example/twainz/SplashActivity.java
@@ -1,33 +1,36 @@
 package com.example.twainz;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashActivity extends Activity {
+import com.example.twainz.MainActivity;
 
-    Handler handler;
+
+public class SplashActivity extends Activity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            try {
-                setContentView(R.layout.splashfile);
-            }catch (Exception e){
-                e.printStackTrace();
-                setContentView(R.layout.splashfile_basic);
-            }
-                //added this basic splash file just because my phone (Zuk Z2 model)
-                // was crashing trying to inflate the image view in that splashfile.xml
-            handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  // Need full screen without title bars for splash screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN );
+        setContentView(R.layout.splash_activity);
+        Thread t = new Thread(){
+            public void run(){
+                try {
+                    Thread.sleep(1000);  // change the time according to your needs(its in milliseconds)
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);     // change the activity you want to load
+                    startActivity(i);
                     finish();
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
-            }, 3000);
+            }
+        };
+        t.start();
     }
 }
